27dba1b38da2e8602465ffbf469d5cbbd17dee04
Ergi <ergishehu615@gmail.com>
2019-08-22 18:12:05 -0400

    I finally did this

--- LOTR-DOM-master/scripts/fellowship.js
+++ LOTR-DOM-master/scripts/fellowship.js
@@ -34,23 +34,24 @@ function makeMiddleEarth() {
   $section.setAttribute("id", "Middle-Earth");
 
   var $article = document.createElement('article');
+  console.log($article)
   var $shire = document.querySelectorAll('article')[0];
-
+  console.log($shire);
   $main.appendChild($section);
 
   // add each land as an article tag (add them one by one in a loop)
-  for (var i = 0 ; i < lands.length ; i++) {
-   var $article = document.createElement('article');
-   // inside each article tag include an h1 with the name of the land
-   var $h1 = document.createElement('h1');
-   $h1.textContent = lands[i];
-   var nameinArray = lands[i].toLowerCase().split(" ");
-   var nameNoSpace = nameinArray.join("-");
+  for (var i = 0; i < lands.length; i++) {
+    var $article = document.createElement('article');
+    // inside each article tag include an h1 with the name of the land
+    var $h1 = document.createElement('h1');
+    $h1.textContent = lands[i];
+    var nameinArray = lands[i].toLowerCase().split(" ");
+    var nameNoSpace = nameinArray.join("-");
 
-   // each article should also have a class equal to it's name ('Mordor' element should have a class of 'mordor', 'The Shire' should have a class of 'the-shire' - HINT: look up .split() and .join() for strings )
-   $article.setAttribute("class", nameNoSpace);
-   $article.appendChild($h1);
-   $section.appendChild($article);
+    // each article should also have a class equal to it's name ('Mordor' element should have a class of 'mordor', 'The Shire' should have a class of 'the-shire' - HINT: look up .split() and .join() for strings )
+    $article.setAttribute("class", nameNoSpace);
+    $article.appendChild($h1);
+    $section.appendChild($article);
   }
 
   // append middle-earth to your document body
@@ -69,7 +70,7 @@ function makeHobbits() {
   $shire.appendChild(list);
 
   // each hobbit should be a list item, with text showing their name
-  for(let i = 0; i < hobbits.length; i ++) {
+  for (let i = 0; i < hobbits.length; i++) {
     let li = document.createElement('li');
     li.innerHTML = hobbits[i];
     list.appendChild(li);
@@ -78,60 +79,120 @@ function makeHobbits() {
   // give each hobbit a class of hobbit
 
 }
- makeHobbits();
+makeHobbits()
 
 
 // Part 3
 function keepItSecretKeepItSafe() {
   // create a div with an id of 'the-ring'
+  let theRing = document.createElement("div");
+  theRing.setAttribute('id', 'the-ring');
   // give the div a class of 'magic-imbued-jewelry'
+  theRing.setAttribute('class', 'magic-imbued-jewelry')
+  console.log(theRing);
   // add the ring as a child of Frodo
+  //hobbits[0].appendChild('theRing');
+  //console.log(hobbits);
 }
-// keepItSecretKeepItSafe();
+keepItSecretKeepItSafe();
 
 
 // Part 4
 function makeBuddies() {
   // create an aside tag
+  let aside = document.createElement('aside');
+
   // attach an unordered list of each 'buddy' in the aside
+  let list = document.createElement('ul')
+  aside.appendChild(list);
+  console.log(aside);
+
+  for (let i = 0; i < buddies.length; i += 1) {
+    let li = document.createElement('li')
+    li.innerHTML = buddies[i];
+    list.appendChild(li);
+  }
   // insert your aside as a child element of rivendell
+  let rivenedell = document.querySelector('.rivendell');
+  rivenedell.appendChild(aside);
+  //console.log(rivenedell);
+
 }
-// makeBuddies();
+makeBuddies();
 
 
 // Part 5
 function beautifulStranger() {
   // change the 'Strider' textnode to 'Aragorn'
+  let change = document.querySelectorAll('article')
+  let changePlace = change[1];
+  let place = changePlace.getElementsByTagName('li');
+
+  for (let i = 0; i < place.length; i += 1) {
+    if (place[i].textContent == 'Strider') {
+      place[i].textContent = 'Aragon';
+    }
+  }
+  //console.log("place", place);
+
 }
-// beautifulStranger();
+beautifulStranger();
 
 
 // Part 6
 function leaveTheShire() {
   // assemble the hobbits and move them to Rivendell
+  let hobbits = document.querySelector('article');
+  let theShire = hobbits.getElementsByClassName('hobbit')
+  let rivendell = document.querySelector('.rivendell').querySelector('ul');
+
+
+  for (let i = 0; i < 4; i += 1) {
+    rivendell.appendChild(theShire[0]);
+  }
+  //console.log('rivendell', rivendell)
   // how does appendChild work on an element that already exists on the page?
 }
-// leaveTheShire();
+leaveTheShire();
 
 
 // Part 7
 function forgeTheFellowShip() {
   // create a new div with an id of 'the-fellowship'
+  let theFellowship = document.createElement("div");
+  theFellowship.setAttribute('id', "the-fellowship");
+
+let rivendell = document.querySelector('.rivendell')
+
+  let li = document.querySelectorAll('li')
+  console.log(li);
+
   // add each hobbit and buddy one at a time to 'the-fellowship'
+for(let i = 0; i < 9; i += 1) {
+  rivendell.appendChild(li[i]);
+}
+
   // after each character is added make an <a href="http://www.w3schools.com/jsref/met_win_alert.asp">alert</a> that they have joined your party
   // append the fellowship div to rivendell
+  rivendell.appendChild(theFellowship);
+  console.log(rivendell);
 }
-// forgeTheFellowShip();
+forgeTheFellowShip();
 
 
 // Part 8
 function theBalrog() {
+let gandalfTheWhite = document.querySelector('li')
+
   // change the 'Gandalf' textNode to 'Gandalf the White'
+  gandalfTheWhite.textContent = "Gandald the White";
   // apply style to the element
+  gandalfTheWhite.style.fontSize = '20px';
   // add a gray 3px border
+
   // use documentation if you're unsure how to add style with javascript!
 }
-// theBalrog();
+theBalrog();
 
 
 // Part 9
@@ -144,7 +205,7 @@ function hornOfGondor() {
 
 
 // Part 10
-function itsDangerousToGoAlone(){
+function itsDangerousToGoAlone() {
   // take Frodo and Sam out of the fellowship and move them to Mordor
   // add a div with an id of 'mount-doom' to Mordor
 }
@@ -158,7 +219,6 @@ function weWantsIt() {
   // Move Gollum into Mount Doom
 }
 // weWantsIt();
-
 
 // Part 12
 function thereAndBackAgain() {
